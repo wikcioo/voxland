9f311e19f4024e063a1071c47da254daa2dd197d
Wiktor Ciolkowski <wiktor.ciolkowski@proton.me>
2024-10-16 00:36:54 +0200

    Create interactive fpv camera

--- client/main.cpp
+++ client/main.cpp
@@ -33,6 +33,16 @@ const char *fragment_shader_source =
     "    out_color = color;\n"
     "}\n";
 
+glm::vec3 camera_position  = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 camera_direction = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 camera_up        = glm::vec3(0.0f, 1.0f, 0.0f);
+float     camera_fov       = 45.0f;
+float     camera_pitch     = 0.0f;
+float     camera_yaw       = -90.0f;
+
+uint32_t current_window_width = WINDOW_WIDTH;
+uint32_t current_window_height = WINDOW_HEIGHT;
+
 static void glfw_error_callback(int code, const char *description)
 {
     fprintf(stderr, "error: glfw error (%d): %s\n", code, description);
@@ -42,6 +52,8 @@ static void glfw_framebuffer_size_callback(GLFWwindow *window, int width, int he
 {
     UNUSED(window);
     glViewport(0, 0, width, height);
+    current_window_width = width;
+    current_window_height = height;
 }
 
 static bool is_polygon_mode = false;
@@ -56,6 +68,44 @@ static void glfw_key_callback(GLFWwindow *window, int key, int scancode, int act
         int32_t mode = is_polygon_mode ? GL_LINE : GL_FILL;
         glPolygonMode(GL_FRONT_AND_BACK, mode);
     }
+}
+
+static void glfw_mouse_moved_callback(GLFWwindow *window, double xpos, double ypos)
+{
+    UNUSED(window);
+
+    static bool first_mouse_move = true;
+    static float last_x, last_y;
+
+    if (first_mouse_move) {
+        last_x = (float) xpos;
+        last_y = (float) ypos;
+        first_mouse_move = false;
+    }
+
+    float xoffset = (float) xpos - last_x;
+    float yoffset = last_y - (float) ypos;
+    last_x = (float) xpos;
+    last_y = (float) ypos;
+
+    static float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    camera_yaw += xoffset;
+    camera_pitch += yoffset;
+
+    if (camera_pitch > 89.0f) {
+        camera_pitch = 89.0f;
+    } else if (camera_pitch < -89.0f) {
+        camera_pitch = -89.0f;
+    }
+
+    glm::vec3 direction;
+    direction.x = glm::cos(glm::radians(camera_yaw)) * glm::cos(glm::radians(camera_pitch));
+    direction.y = glm::sin(glm::radians(camera_pitch));
+    direction.z = glm::sin(glm::radians(camera_yaw)) * glm::cos(glm::radians(camera_pitch));
+    camera_direction = glm::normalize(direction);
 }
 
 uint32_t create_shader(uint32_t type, const char *source)
@@ -100,6 +150,24 @@ uint32_t create_program(uint32_t vertex_shader, uint32_t fragment_shader)
     return program;
 }
 
+void process_input(GLFWwindow *window, float delta_time)
+{
+    static float velocity = 5.0f;
+    float speed = delta_time * velocity;
+
+    glm::vec3 camera_right = glm::normalize(glm::cross(camera_direction, camera_up));
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        camera_position += camera_direction * speed;
+    } else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        camera_position -= camera_direction * speed;
+    } else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        camera_position -= camera_right * speed;
+    } else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        camera_position += camera_right * speed;
+    }
+}
+
 int main(void)
 {
     printf("Hello, Voxland Client\n");
@@ -130,6 +198,9 @@ int main(void)
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, glfw_framebuffer_size_callback);
     glfwSetKeyCallback(window, glfw_key_callback);
+    glfwSetCursorPosCallback(window, glfw_mouse_moved_callback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     uint32_t err = glewInit();
     if (err != GLEW_OK) {
@@ -213,16 +284,19 @@ int main(void)
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (const void *) 0);
     glEnableVertexAttribArray(0);
 
-    glm::vec3 camera_position = glm::vec3(1.0f, 2.0f, 3.0f);
-    glm::vec3 camera_direction = glm::normalize(glm::vec3(-1.0f, -2.0f, -3.0f));
-    glm::vec3 camera_up = glm::vec3(0.0f, 1.0f, 0.0f);
-    float fov = 45.0f;
-
-    glm::mat4 projection = glm::perspective(glm::radians(fov), (float) WINDOW_WIDTH / (float) WINDOW_HEIGHT, 0.1f, 100.0f);
-    glm::mat4 view = glm::lookAt(camera_position, camera_position + camera_direction, camera_up);
-    glm::mat4 model = glm::mat4(1.0f);
+    float delta_time = 0.0;
+    float last_time = 0.0;
 
     while (!glfwWindowShouldClose(window)) {
+        float now = (float) glfwGetTime();
+        delta_time = now - last_time;
+        last_time = now;
+
+        process_input(window, delta_time);
+        glm::mat4 projection = glm::perspective(glm::radians(camera_fov), (float) current_window_width / (float) current_window_height, 0.1f, 100.0f);
+        glm::mat4 view = glm::lookAt(camera_position, camera_position + camera_direction, camera_up);
+        glm::mat4 model = glm::mat4(1.0f);
+
         glClearColor(0.384f, 0.384f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
