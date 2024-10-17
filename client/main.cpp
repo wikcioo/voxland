b886d4eee8ac95ba6836f2d2c4b5d129b9ef0e84
Wiktor Ciolkowski <wiktor.ciolkowski@proton.me>
2024-10-17 19:30:31 +0200

    Connect client to the server

--- client/main.cpp
+++ client/main.cpp
@@ -1,16 +1,36 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 #include <dlfcn.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <poll.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <errno.h>
+#include <signal.h>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include "log.h"
+#include "asserts.h"
 #include "defines.h"
 #include "game.h"
+#include "hexdump.h"
 
-Game game = {};
+#define POLLFD_COUNT 1
+#define INPUT_BUFFER_SIZE 1024
+#define POLL_INFINITE_TIMEOUT -1
+
+LOCAL Game game = {};
+LOCAL i32 client_socket;
+LOCAL struct pollfd pfds[POLLFD_COUNT];
+LOCAL bool running = false;
+LOCAL pthread_t network_thread;
 
 const char *libgame_filename = "libgame.so";
 void *libgame = NULL;
@@ -118,8 +138,174 @@ LOCAL void glfw_mouse_moved_callback(GLFWwindow *window, f64 xpos, f64 ypos)
     game.camera_direction = glm::normalize(direction);
 }
 
-int main(void)
+LOCAL void signal_handler(i32 sig)
 {
+    UNUSED(sig);
+    running = false;
+}
+
+LOCAL void handle_incoming_server_data(void)
+{
+    u8 buffer[INPUT_BUFFER_SIZE] = {0};
+    i64 bytes_read = recv(client_socket, buffer, INPUT_BUFFER_SIZE, 0);
+    if (bytes_read <= 0) {
+        if (bytes_read == 0) {
+            LOG_INFO("orderly shutdown: disconnected from server\n");
+            close(client_socket);
+            exit(EXIT_FAILURE);
+        } else if (bytes_read == -1) {
+            LOG_ERROR("recv error: %s\n", strerror(errno));
+        }
+        return;
+    }
+
+#if 1
+    LOG_DEBUG("read %lld bytes of data:\n", bytes_read);
+    hexdump(stdout, buffer, bytes_read, HEXDUMP_FLAG_CANONICAL);
+#endif
+}
+
+LOCAL void *handle_networking(void *args)
+{
+    UNUSED(args);
+
+    while (running) {
+        i32 num_events = poll(pfds, POLLFD_COUNT, POLL_INFINITE_TIMEOUT);
+
+        if (num_events == -1) {
+            if (errno == EINTR) {
+                LOG_TRACE("interrupted 'poll' system call\n");
+                break;
+            }
+            LOG_FATAL("poll error: %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
+        for (i32 i = 0; i < POLLFD_COUNT; i++) {
+            if (pfds[i].revents & POLLIN) {
+                if (pfds[i].fd == client_socket) {
+                    handle_incoming_server_data();
+                }
+            }
+        }
+    }
+
+    if (close(client_socket) == -1) {
+        LOG_ERROR("error while closing the socket: %s\n", strerror(errno));
+    } else {
+        LOG_INFO("closed client socket\n");
+    }
+
+    return NULL;
+}
+
+LOCAL bool connect_to_server(const char *ip_addr, const char *port)
+{
+    struct addrinfo hints = {}, *result = NULL, *rp = NULL;
+
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    i32 rc = getaddrinfo(ip_addr, port, &hints, &result);
+    if (rc != 0) {
+        LOG_ERROR("getaddrinfo error: %s\n", gai_strerror(rc));
+        return false;
+    }
+
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        client_socket = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (client_socket == -1) {
+            continue;
+        }
+
+        if (connect(client_socket, rp->ai_addr, rp->ai_addrlen) == -1) {
+            LOG_ERROR("connect error: %s\n", strerror(errno));
+            continue;
+        }
+
+        break;
+    }
+
+    freeaddrinfo(result);
+
+    if (rp == NULL) {
+        return false;
+    }
+
+    pfds[0].fd = client_socket;
+    pfds[0].events = POLLIN;
+
+    struct sigaction sa;
+    sa.sa_flags = SA_RESTART;
+    sa.sa_handler = &signal_handler;
+    sigaction(SIGINT, &sa, NULL);
+
+    pthread_create(&network_thread, NULL, handle_networking, NULL);
+
+    return true;
+}
+
+LOCAL char *shift(int *argc, char ***argv)
+{
+    ASSERT(*argc > 0);
+    char *result = **argv;
+    *argc -= 1;
+    *argv += 1;
+    return result;
+}
+
+LOCAL void usage(FILE *stream, const char *const program)
+{
+    fprintf(stream, "usage: %s -ip <server ip address> -p <port> [-h]\n", program);
+}
+
+int main(int argc, char **argv)
+{
+    const char *const program = shift(&argc, &argv);
+    const char *server_ip_address = NULL;
+    const char *server_port = NULL;
+
+    while (argc > 0) {
+        const char *flag = shift(&argc, &argv);
+
+        if (strcmp(flag, "-ip") == 0) {
+            if (argc == 0) {
+                LOG_FATAL("missing argument for flag `%s`\n", flag);
+                usage(stderr, program);
+                exit(EXIT_FAILURE);
+            }
+
+            server_ip_address = shift(&argc, &argv);
+        } else if (strcmp(flag, "-p") == 0) {
+            if (argc == 0) {
+                LOG_FATAL("missing argument for flag `%s`\n", flag);
+                usage(stderr, program);
+                exit(EXIT_FAILURE);
+            }
+
+            server_port = shift(&argc, &argv);
+        } else if (strcmp(flag, "-h") == 0) {
+            usage(stdout, program);
+            exit(EXIT_SUCCESS);
+        } else {
+            LOG_FATAL("unknown flag `%s`\n", flag);
+            usage(stderr, program);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (server_ip_address == NULL) {
+        LOG_FATAL("server ip address argument missing\n");
+        usage(stderr, program);
+        exit(EXIT_FAILURE);
+    }
+
+    if (server_port == NULL) {
+        LOG_FATAL("server port argument missing\n");
+        usage(stderr, program);
+        exit(EXIT_FAILURE);
+    }
+
     if (!reload_libgame()) {
         LOG_FATAL("failed to reload libgame\n");
         exit(EXIT_FAILURE);
@@ -176,9 +362,18 @@ int main(void)
         LOG_INFO("  - glsl version: %s\n", glsl_version);
     }
 
+    running = true;
+
+    if (connect_to_server(server_ip_address, server_port)) {
+        LOG_INFO("successfully connected to server at %s:%s\n", server_ip_address, server_port);
+    } else {
+        LOG_FATAL("failed to connect to server at %s:%s\n", server_ip_address, server_port);
+        exit(EXIT_FAILURE);
+    }
+
     game_init(&game);
 
-    while (!glfwWindowShouldClose(game.window)) {
+    while (!glfwWindowShouldClose(game.window) && running) {
         f32 now = (f32) glfwGetTime();
         game.delta_time = now - game.last_time;
         game.last_time = now;
@@ -191,5 +386,8 @@ int main(void)
     glfwDestroyWindow(game.window);
     glfwTerminate();
 
-    return 0;
+    pthread_kill(network_thread, SIGINT);
+    pthread_join(network_thread, NULL);
+
+    return EXIT_SUCCESS;
 }
